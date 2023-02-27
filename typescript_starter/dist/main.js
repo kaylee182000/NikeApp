4b40c3de3bfe1aaba88840c4cfc5e7f86bee23da
kaylee182000 <trungkien40414@gmail.com>
2023-02-27 20:33:55 +0700

    understand in tsconfig

---
+++ typescript_starter/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isPositive = void 0;
+const isPositive = (n) => {
+    return n > 0;
+};
+exports.isPositive = isPositive;
--- typescript_starter/index.ts
+++
@@ -1,3 +0,0 @@
-const myName: string = 'Kien';
-
-console.log('My name: ' + myName);
