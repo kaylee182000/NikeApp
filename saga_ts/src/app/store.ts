916770e785a4577b46b812eb9e5c8fc50c25b48b
kaylee182000 <trungkien40414@gmail.com>
2023-03-03 15:25:53 +0700

    learn saga

--- saga_ts/src/app/store.ts
+++ saga_ts/src/app/store.ts
@@ -1,16 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
-
+import createSagaMiddleware from '@redux-saga/core'
 import counterReducer from '../features/counter/counterSlice'
 import { docsApi } from '../services/docs'
+import rootSaga from './rootSaga'
 
+//Buoc tao middleware
+const sagaMiddleware = createSagaMiddleware()
+
+//add middleware vao redux toolkit
 export const store = configureStore({
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(docsApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware, docsApi.middleware),
   reducer: {
     counter: counterReducer,
     [docsApi.reducerPath]: docsApi.reducer,
   },
+  devTools: true,
 })
+
+//khoi chay saga
+sagaMiddleware.run(rootSaga)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
