eca48a467fa95817841c2046f415ce8677664796
kaylee182000 <trungkien40414@gmail.com>
2023-04-08 21:42:01 +0700

    delete some file

--- typescript_starter/dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPositive = void 0;
-const isPositive = (n) => {
-    return n > 0;
-};
-exports.isPositive = isPositive;
--- typescript_starter/src/lectures/27-4/main.ts
+++
@@ -1,3 +0,0 @@
-export const isPositive = (n: number) => {
-  return n > 0;
-};
