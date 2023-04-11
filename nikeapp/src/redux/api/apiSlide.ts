9a9997ae9337f597c3a67f6f7b8d215b9d76fe0c
kaylee182000 <trungkien40414@gmail.com>
2023-04-11 22:53:22 +0700

    change the default url of app

--- nikeapp/src/redux/api/apiSlide.ts
+++ nikeapp/src/redux/api/apiSlide.ts
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 //use the ip address from the machine using ipconfig
-const baseUrl = 'http://192.168.1.10:8080/api/v1/';
+const baseUrl = 'https://nikestore-kaylee.onrender.com/api/v1/';
 
 // Define a service using a base URL and expected endpoints
 export const apiSlide = createApi({
